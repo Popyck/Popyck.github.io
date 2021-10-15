2b7e565334ac38f87595d2d78cc397621e9cc6d0
Popyck <92472514+Popyck@users.noreply.github.com>
2021-10-15 22:33:42 +0600

    в

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+///alert('Вы точно готовы  , увидеть этот шедевр ? Если да тогда жми (OK)' )
+///"use strict"
+///let currentUserName = prompt("Добрый день! Как Вас зовут?");
+  ///  let chekUserName = confirm(`То есть Вас зовут ${currentUserName}?`);
+    ///   if (chekUserName == true) {
+   // alert(`Спасибо, что прошли окно авторизации ${currentUserName}. Всего Вам хорошего!`);
+///} else {
+///alert(`Перезагрузите страницу и пройдите данный небольшой опрос снова...`);
+//}
